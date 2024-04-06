a223259b9df9441d5b1991ec5e147b116ed671af
PiyushRepos <165693211+PiyushRepos@users.noreply.github.com>
2024-04-06 11:16:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,3 @@
-// console.log("Welcome to Tic Tac Toe")
-// let music = new Audio("music.mp3")
-// let audioTurn = new Audio("ting.mp3")
-// let gameover = new Audio("gameover.mp3")
-// let turn = "X"
-// let isgameover = false;
-
-// // Function to change the turn
-// const changeTurn = ()=>{
-//     return turn === "X"? "0": "X"
-// }
-
-// // Function to check for a win
-// const checkWin = ()=>{
-//     let boxtext = document.getElementsByClassName('boxtext');
-//     let wins = [
-//         [0, 1, 2, 5, 5, 0],
-//         [3, 4, 5, 5, 15, 0],
-//         [6, 7, 8, 5, 25, 0],
-//         [0, 3, 6, -5, 15, 90],
-//         [1, 4, 7, 5, 15, 90],
-//         [2, 5, 8, 15, 15, 90],
-//         [0, 4, 8, 5, 15, 45],
-//         [2, 4, 6, 5, 15, 135],
-//     ]
-//     wins.forEach(e =>{
-//         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-//             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-//             isgameover = true
-//             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-//             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-//             document.querySelector(".line").style.width = "20vw";
-//         }
-//     })
-// }
-
-// // Game Logic
-// // music.play()
-// let boxes = document.getElementsByClassName("box");
-// Array.from(boxes).forEach(element =>{
-//     let boxtext = element.querySelector('.boxtext');
-//     element.addEventListener('click', ()=>{
-//         if(boxtext.innerText === ''){
-//             boxtext.innerText = turn;
-//             turn = changeTurn();
-//             audioTurn.play();
-//             checkWin();
-//             if (!isgameover){
-//                 document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-//             }
-//         }
-//     })
-// })
-
-// // Add onclick listener to reset button
-// reset.addEventListener('click', ()=>{
-//     let boxtexts = document.querySelectorAll('.boxtext');
-//     Array.from(boxtexts).forEach(element => {
-//         element.innerText = ""
-//     });
-//     turn = "X";
-//     isgameover = false
-//     document.querySelector(".line").style.width = "0vw";
-//     document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-//     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-// })
-
 let x_class = "X";
 let o_class = "O";
 let x_turn = true;
@@ -92,7 +25,6 @@ function startGame() {
   allCells.forEach((cell) => {
     cell.addEventListener("click", handleClick, { once: true });
   });
-  setBoardHoverClass();
 }
 
 const audio = new Audio('music.mp3');
@@ -108,23 +40,11 @@ function handleClick(e) {
     showWinner(true);
   } else {
     swapTurn();
-    setBoardHoverClass();
   }
 }
 
 function swapTurn() {
   x_turn = !x_turn;
-}
-
-function setBoardHoverClass() {
-  board.classList.remove(x_class);
-  board.classList.remove(o_class);
-
-  if (x_turn) {
-    board.classList.add(x_class);
-  } else {
-    board.classList.add(o_class);
-  }
 }
 
 function checkWin(currentClass) {
